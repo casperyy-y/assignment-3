f6d016893c212e8c7ae4f4f21b13348a92134559
casperyy-y <hl96770@mail.harpercollege.edu>
2025-03-04 05:56:18 +0800

    Create PS3P5.py

---
+++ PS3P5.py
@@ -0,0 +1,9 @@
+pi = 3.174
+
+radius = float(input("Enter the radius of the circle "))
+
+area = pi * (radius ** 2)
+perimeter = 2 * pi * radius
+
+print("Area: "， area)
+print("Perimeter: ", perimeter)
